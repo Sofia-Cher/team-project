bcc89bcf811d2eba0db700e5a7477d8b96b0fc6b
Sofia Chernichenko <sofcern@yandex.ru>
2026-09-19 21:01:00 +0700

    feat: добавлены файлы функций варианта 70 (degToRad, radToDeg)

---
+++ chernichenko.h
@@ -0,0 +1,11 @@
+#ifndef CHERNICHENKO_H
+#define CHERNICHENKO_H
+
+// Функции варианта 70 (Черниченко С.)
+// Перевод градусов в радианы
+double degToRad(double d);
+
+// Перевод радиан в градусы
+double radToDeg(double r);
+
+#endif // CHERNICHENKO_H
